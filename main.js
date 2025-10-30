2e69e8a1994433311c62fbca09815c41681d0c2e
victormaiajunior-beep <victor.maia.junior@escola.pr.gov.br>
2025-10-30 09:39:43 -0300

    h

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const avanca = document.querySelectorAll("".bth-proximo")
     console.log(avanca)
 
     avanca.forEach(button =>{
+        button.addEventListener('clik', function(){
+            const atual = document.querySelector('.ativo')
+            const proximoPasso = "passo-" + this.getAttribute('data-proximo
 
    
     })
